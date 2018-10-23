e81fd1ceebde7477516b48faa46dc5597468512d
Shuntaro Katsuda <shunten3@gmail.com>
2018-10-23 17:28:00 +0900

    update code handout

--- lib/main.c
+++ lib/main.c
@@ -3,13 +3,11 @@
 
 extern int _toplevel();
 
-void *mymalloc(int n)
-{
+void *mymalloc(int n) {
   return malloc(n * sizeof(int));
 }
 
-int main()
-{
+int main() {
   printf("%d\n", _toplevel());
   return 0;
 }
